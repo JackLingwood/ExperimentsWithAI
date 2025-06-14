308a9dc50acfbb26ad385d9b1cb302ccfe48fd07
Jack Lingwood <jlingwood@fieldca.com>
2025-06-13 22:27:37 -0400

    Basic iteration in Python

---
+++ BasicPython/iteration.py
@@ -0,0 +1,23 @@
+even = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+print("even = [0,2,4,6,8,10,12,14,16,18,20]")
+print(even)
+
+# This code demonstrates the use of lists and iteration in Python.
+print("Printing even numbers from the list:")
+for n in even:
+    print(n, end=" ")
+
+# This code demonstrates the use of a while loop to print even numbers from 0 to 20.
+# It initializes a variable x to 0 and increments it by 2 in each iteration until it reaches 20.
+print("\nPrinting even numbers from 0 to 20 using a while loop:")
+x=0
+while x<=20:
+    print(x,end=" ")
+    x = x + 2
+
+# This code demonstrates the use of a while loop to print even numbers from 0 to 20.
+print("\nPrinting even numbers from 0 to 20 using a while loop:")
+x=0
+while x<=20:
+    print(x,end=" ")
+    x += 2
