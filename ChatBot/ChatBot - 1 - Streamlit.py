3bf117b0f2bbc5e2c26d319be763411f81b974bc
Jack Lingwood <fq123321qf@gmail.com>
2025-07-07 13:38:49 -0400

    ChatBot - Remove parts not recognized by streamlit Python environment

--- ChatBot/ChatBot - 1 - Streamlit.py
+++ ChatBot/ChatBot - 1 - Streamlit.py
@@ -1,37 +1,37 @@
-# Working with Streamlit
-# https://streamlit.io/ 
-#  
-# ---------------------------------------------------------------------------------------------
-from dotenv import load_dotenv, find_dotenv
-import os
-import sys
+# # Working with Streamlit
+# # https://streamlit.io/ 
+# #  
+# # ---------------------------------------------------------------------------------------------
+# from dotenv import load_dotenv, find_dotenv
+# import os
+# import sys
 
-# Add Shared folder to sys.path
-sys.path.append(os.path.abspath("Shared"))
+# # Add Shared folder to sys.path
+# sys.path.append(os.path.abspath("Shared"))
 
-from utils import heading, heading2, heading3, clearConsole, note, print_ascii_table , highlight_differences_diff_based
+# from utils import heading, heading2, heading3, clearConsole, note, print_ascii_table , highlight_differences_diff_based
 
-from openai import OpenAI
+# from openai import OpenAI
 
-# Load environment variables
-load_dotenv(find_dotenv(), override = True)
+# # Load environment variables
+# load_dotenv(find_dotenv(), override = True)
 
-# Clear console and set working directory
-clearConsole()
+# # Clear console and set working directory
+# clearConsole()
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-os.chdir(script_dir)
-print("Current working directory:", os.getcwd())
+# script_dir = os.path.dirname(os.path.abspath(__file__))
+# os.chdir(script_dir)
+# print("Current working directory:", os.getcwd())
 
-# Headings
-name = os.path.basename(__file__)
-heading(f"{name}")
+# # Headings
+# name = os.path.basename(__file__)
+# heading(f"{name}")
 
-# Environment variables
-api_key = os.environ.get("api_key")
-pinecone_vector_database_key = os.environ.get("pinecone_vector_database_key")
-pinecone_environment = os.environ.get("pinecone_environment", "gcp-starter")
-# ---------------------------------------------------------------------------------------------
+# # Environment variables
+# api_key = os.environ.get("api_key")
+# pinecone_vector_database_key = os.environ.get("pinecone_vector_database_key")
+# pinecone_environment = os.environ.get("pinecone_environment", "gcp-starter")
+# # ---------------------------------------------------------------------------------------------
 
 
 import streamlit as st
